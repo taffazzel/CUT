1e63fd2f26849be735e49b397930a784eed2e9ff
taffazzel <tafaz_hossain@cloudwick.com>
2018-01-02 14:49:25 -0500

    initial commit

---
+++ kafka_producer_twitter/kafka_producer_twitter.py
@@ -0,0 +1,7 @@
+from pyspark import SparkContext
+from pyspark import SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql.context import SQLContext
+import tweepy
+import twitter_config
