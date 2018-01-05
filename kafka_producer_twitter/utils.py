2999b0cdd4cb2b1757b3f16a193c65b6fd0ada1e
Taffazzel <tafaz_hossain@yahoo.com>
2018-01-05 18:56:50 -0500

    Added twitter handle

---
+++ kafka_producer_twitter/utils.py
@@ -0,0 +1,11 @@
+
+tweet_handles = { '@BreakingNews': 'Breaking News',
+		  '@BBCBreaking' : 'BBC Breaking News',
+		  '@cnnbrk' : 'CNN Breaking',
+		  '@WSJbreakingnews' : 'Wall Street Breaking News',
+		  '@ReutersLive' : 'Reuters',
+		  '@CBSTopNews' : 'CBS Top News',
+		  '@AJELive' : 'Al Jazeera',
+		  '@SkyNewsBreak' : 'Sky News',
+		  '@TWCBreaking' : 'The Weather Channel'
+		}
