f132c12d4e60ea490dd8174d735451210a5c07f8
Taffazzel <tafaz_hossain@yahoo.com>
2018-01-13 20:04:35 -0500

    port number adde

--- kafka_consumer_twitter/kafka_reciever.py
+++ kafka_consumer_twitter/kafka_reciever.py
@@ -8,7 +8,7 @@ if __name__=="__main__":
 	conf = SparkConf(appName)
 	sc = SparkContext(conf=conf)
 	ssc = StreamingContext(sc,10)
-	kvs = KafkaUtils.createStream(ssc,'localhost:9092,node3.shinigami.com',"ttt",{'CUT':1})
+	kvs = KafkaUtils.createStream(ssc,'localhost:9092,node3.shinigami.com:9092',"ttt",{'CUT':1})
 	lines = kvs.map(lambda x: x[1])
     	counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
 	counts.pprint()
