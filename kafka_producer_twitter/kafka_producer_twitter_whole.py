af5fff9fbec517a3d672d8c45676fc10b75311e4
Taffazzel <tafaz_hossain@yahoo.com>
2018-01-15 17:07:57 -0500

    whole twitter

---
+++ kafka_producer_twitter/kafka_producer_twitter_whole.py
@@ -0,0 +1,58 @@
+import json
+import confluent_kafka 
+from kafka import KafkaProducer, KafkaClient
+
+import tweepy
+from tweepy import OAuthHandler, Stream, API
+from tweepy.streaming import StreamListener
+import twitter_config
+from utils import tweet_handles
+
+consumer_key = twitter_config.consumer_key
+consumer_secret = twitter_config.consumer_secret
+access_token = twitter_config.access_token
+access_token_secret = twitter_config.access_token_secret
+
+bootstrap_servers = 'node1.shinigami.com:6667'
+topic = b'CUT'
+conf = {'bootstrap.servers': bootstrap_servers}
+producer = confluent_kafka.Producer(**conf)
+
+
+class TweetStreamListener(StreamListener):
+    def __init__(self,api):
+        self.api = api
+        super(StreamListener, self).__init__()
+
+    def on_data(self, data):
+        """
+        This is called when new data arrives as live stream
+        """
+        try:
+            producer.produce(topic, data.encode('utf-8'))
+            print ("In the TRY")
+        except Exception as e:
+            print (e)
+            return False
+        return True
+
+    def on_error(self, status_code):
+        print ("Error received in kafka producer, ", status_code)
+        return True #don't kill the stream
+
+    def on_timeout(self):
+        return True
+
+if __name__ == '__main__':
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = API(auth)
+
+    stream = Stream(auth, listener=TweetStreamListener(api))
+    all_handles = []
+    for handle in tweet_handles.keys():
+        all_handles.extend(handle)
+
+    stream.filter(track=all_handles,languages=["en"])
+
+
