d0f5c5153b742366ff466a8bac6f0d035db5ab39
Taffazzel <tafaz_hossain@yahoo.com>
2018-01-19 15:35:34 -0500

    writing to elastic search is not working

--- kafka_producer_twitter/kafka_consumer.py
+++ kafka_producer_twitter/kafka_consumer.py
@@ -1,3 +1,4 @@
+
 #pyspark --master yarn --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1
 # -*- coding: utf-8 -*-
 
@@ -11,6 +12,7 @@ from pyspark.streaming.kafka import KafkaUtils
 import json  
 from pyspark.conf import SparkConf
 #from elasticsearch import Elasticsearch
+from pyspark_elastic import EsSparkContext
 
 if __name__=='__main__':
 	topic = b'CUT'
@@ -22,12 +24,13 @@ if __name__=='__main__':
 	parsed = directKafkaStream.map(lambda v:json.loads(v[1]))
 	#wcount = parsed.flatMap(lambda line: line.split(" ")).map(lambda w:(w,1)).reduceByKey(lambda a,b:a+b)
 	#parsed.map(lambda tweet: tweet.keys()).pprint()
-	#parsed.filter(lambda s:len(s)>2).map(lambda tweet: tweet).pprint()
-	
+	#parsed.filter(lambda s:len(s)>2).map(lambda tweet: tweet).pprint()		
 	text = parsed.filter(lambda s:len(s)>2).map(lambda tweet: tweet['text'].encode('utf-8'))
 	user = parsed.filter(lambda s:len(s)>2).map(lambda tweet: tweet['user']['screen_name'])
 	text.pprint()
-	es_conf = {"es.nodes" : "elk1.shinigami.com","es.port" : "9200","es.nodes.client.only" : "true","es.resource" : "cut/type"}
+	#es_conf = {"es.nodes" : "elk1.shinigami.com","es.port" : "9200","es.nodes.client.only" : "true","es.resource" : "cut/internal"}
+	#text.saveAsNewAPIHadoopFile(path='-',outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",keyClass="org.apache.hadoop.io.NullWritable",valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable", conf=es_conf)
+	text.foreachRDD(saveToEs("cut/type"))
 	#text.flatMap(lambda s: s.split(" ")).map(lambda w:(w,1)).reduceByKey(lambda x,y:x+y).pprint()
 	user.pprint()	
 	parsed.count().map(lambda x:'Tweets in this batch: %s' % x).pprint()
